1e5ec076d7dd91b1b620f6ee0d7af4458b25382b
Victor Fernandes <victor.fernandes2004@gmail.com>
2024-12-24 16:39:31 -0400

    Adicionada a classe DaoFactory para centralizar a criação de instâncias DAO, facilitando a injeção de dependências e o gerenciamento de conexões com o banco de dados

--- src/model/dao/DaoFactory.java
+++ src/model/dao/DaoFactory.java
@@ -1,5 +1,27 @@
 package model.dao;
 
-public class DaoFactory {
+import db.DB;
+import model.dao.impl.LojaDaoJDBC;
+import model.dao.impl.PrecoDaoJDBC;
+import model.dao.impl.ProdutoDaoJDBC;
+import model.dao.impl.RelatorioDaoJDBC;
 
+public class DaoFactory {
+	
+	public static LojaDAO createLojaDao() {
+		return new LojaDaoJDBC(DB.getConnection());
+	}
+	
+	public static PrecoDAO createPrecoDao() {
+		return new PrecoDaoJDBC(DB.getConnection());
+	}
+	
+	public static ProdutoDAO createProdutoDao() {
+		return new ProdutoDaoJDBC(DB.getConnection());
+	}
+	
+	public static RelatorioDAO createRelatorioDao() {
+		return new RelatorioDaoJDBC(DB.getConnection());
+	}
+	
 }
